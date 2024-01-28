0037e7168dcdc14cee8467109454d4b17ce1f808
Viswanadh Kandala <savitha.viswanadh@research.iiit.ac.in>
2024-01-28 21:28:27 +0800

    add setup and model downloader

---
+++ model.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+# get the current directory
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+# download a text file using curl without any output
+curl -s -o $DIR/models_list.txt https://raw.githubusercontent.com/VIS-WA/LLM-replication/main/models_list.csv
+# curl -L https://raw.githubusercontent.com/VIS-WA/LLM-replication/main/models_list.csv -o models_list.txt
+
+# print the first two columns of the text file with a separation of 2 tabs
+awk -F, '{print "\n" $1 "\t" $2}' models_list.txt
+
+# user input for a number
+echo -e "\n"
+echo "Enter the S.No of the model to be downloaded: "
+
+read model_no
+
+#add 1 to the user input
+model_no=$((model_no+1))
+model_name=$(awk -F, -v var="$model_no" 'NR==var {print $2}' models_list.txt)
+# fetch the url of the model corresponding to the S.No in column 3
+model_url=$(awk -F, -v var="$model_no" 'NR==var {print $3}' models_list.txt)
+
+
+# print message that name of the model being downloaded
+echo "Downloading $model_name..."
+# echo $model_url
+# echo "$DIR/$model_name.gguf"
+# download the model
+curl -L -o "$DIR/$model_name.gguf" $model_url
---
+++ setup.sh
@@ -0,0 +1,18 @@
+# clone a repo at the same location of this script
+
+# get the current directory
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+# clone the repo at the same location of this script and if the repo is already cloned, then update it
+if [ -d "$DIR/llama.cpp" ]; then
+    cd $DIR/llama.cpp
+    make clean
+    git pull
+else
+    git clone https://github.com/ggerganov/llama.cpp.git $DIR/llama.cpp
+    cd $DIR/llama.cpp
+# git clone https://github.com/ggerganov/llama.cpp.git $DIR/llama.cpp 
+fi
+
+# run make and report errors
+make || exit 1
